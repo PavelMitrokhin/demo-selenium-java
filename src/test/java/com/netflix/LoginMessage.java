44a73c03effb6b558ba04c4d797a30e157091e03
Pavel <pamitrovel@gmail.com>
2024-10-27 13:44:34 +0300

    add LoginMessage with xpath to invalid password and email messages

---
+++ src/test/java/com/netflix/LoginMessage.java
@@ -0,0 +1,6 @@
+package com.netflix;
+
+public class LoginMessage {
+    public static final String INVALID_EMAIL_OR_PHONE_NUMBER = "//*[@id=\":r1:\"]";
+    public static final String INVALID_PASSWORD = "//*[@id=\":r4:\"]";
+}
