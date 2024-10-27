759389d4ecafcc64b33da643f09a6d17a4caf3f5
Pavel <pamitrovel@gmail.com>
2024-10-27 22:52:39 +0300

    refactor ERROR_NO_PHONE_NUMBER

--- src/test/java/by/mall/LoginMessage.java
+++ src/test/java/by/mall/LoginMessage.java
@@ -4,6 +4,5 @@ public class LoginMessage {
     public static final String ERROR_NO_CREDENTIALS = "Поле \"Номер телефона\" обязательно для заполнения.\n" +
             "Поле \"Пароль\" обязательно для заполнения.";
     public static final String ERROR_NO_PASSWORD = "Поле \"Пароль\" обязательно для заполнения.";
-    public static final String ERROR_NO_PHONE_NUMBER = "Значение поля \"Номер телефона\" должно начинаться" +
-            " с +375 затем код (25|29|33|44) и далее 7 цифр (первая из которых не 0)";
+    public static final String ERROR_NO_PHONE_NUMBER = "Поле \"Номер телефона\" обязательно для заполнения.";
 }
