c63fc32a1f507bb1d44bc1954b4eaf78d42f2ac8
Pavel <pamitrovel@gmail.com>
2024-10-27 13:36:58 +0300

    add LoginPage with click Sign In method

---
+++ src/test/java/com/netflix/LoginPage.java
@@ -0,0 +1,19 @@
+package com.netflix;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class LoginPage {
+    private WebDriver driver;
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void clickButtonSignIn() {
+        By buttonSignInBy = By.xpath(LoginXpath.BUTTON_SIGN_IN_XPATH);
+        WebElement buttonSignInWebElement = driver.findElement(buttonSignInBy);
+        buttonSignInWebElement.click();
+    }
+}
