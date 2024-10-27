5a648401e0530faee3a6846c4d8a38263e2b7db4
Pavel <pamitrovel@gmail.com>
2024-10-27 22:20:40 +0300

    add LoginMessage

---
+++ src/test/java/by/mall/LoginMessage.java
@@ -0,0 +1,10 @@
+package by.mall;
+
+public class LoginMessage {
+    public static final String ERROR_NO_CREDENTIALS = "Значение поля \"Номер телефона\" " +
+            "должно начинаться с +375 затем код (25|29|33|44) и далее 7 цифр (первая из которых не 0)\n" +
+            "Поле \"Пароль\" обязательно для заполнения.";
+    public static final String ERROR_NO_PASSWORD = "Поле \"Пароль\" обязательно для заполнения.";
+    public static final String ERROR_NO_PHONE_NUMBER = "Значение поля \"Номер телефона\" должно начинаться" +
+            " с +375 затем код (25|29|33|44) и далее 7 цифр (первая из которых не 0)";
+}
