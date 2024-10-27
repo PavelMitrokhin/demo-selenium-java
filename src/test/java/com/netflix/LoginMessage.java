3468e0745736910ab7d5ab2212da1190aca98df3
Pavel <pamitrovel@gmail.com>
2024-10-27 16:25:52 +0300

    add INVALID_PASSWORD

--- src/test/java/com/netflix/LoginMessage.java
+++ src/test/java/com/netflix/LoginMessage.java
@@ -1,6 +1,7 @@
 package com.netflix;
 
 public class LoginMessage {
-    public static final String INVALID_EMAIL_OR_PHONE_NUMBER = "Укажите действительный адрес электронной почты или номер телефона.";
-    public static final String INVALID_PASSWORD = "Пароль должен содержать от 4 до 60 символов.";
+    public static final String NO_EMAIL_OR_PHONE_NUMBER = "Укажите действительный адрес электронной почты или номер телефона.";
+    public static final String NO_PASSWORD = "Пароль должен содержать от 4 до 60 символов.";
+    public static final String INVALID_PASSWORD = "Неверный пароль для ";
 }
