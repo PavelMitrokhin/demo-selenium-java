daf134b36b46d0f1a4a29f4a0a0d90f1210e0f8e
Pavel <pamitrovel@gmail.com>
2024-10-27 16:21:27 +0300

    add OUTPUT_INVALID_PASSWORD_XPATH

--- src/test/java/com/netflix/LoginXpath.java
+++ src/test/java/com/netflix/LoginXpath.java
@@ -5,7 +5,10 @@ public class LoginXpath {
     public static final String INPUT_EMAIL_OR_PHONE_NUMBER_XPATH = "//*[@id=\":r0:\"]";
     public static final String INPUT_PASSWORD_XPATH = "//*[@id=\":r3:\"]";
 
-    public static final String INPUT_EMPTY_EMAIL_OR_PHONE_NUMBER_XPATH = "//*[@id=\":r1:\"]";
-    public static final String INPUT_EMPTY_PASSWORD_XPATH = "//*[@id=\":r4:\"]";
+    public static final String OUTPUT_EMPTY_EMAIL_OR_PHONE_NUMBER_XPATH = "//*[@id=\":r1:\"]";
+    public static final String OUTPUT_EMPTY_PASSWORD_XPATH = "//*[@id=\":r4:\"]";
+
+    public static final String OUTPUT_INVALID_PASSWORD_XPATH = "//*[@id=\"appMountPoint\"]/div/div/div[2]/div/header/div/div/div/div";
+
 
 }
