83885195ff507413f9d16c1ca1e2117a8ba938ff
Pavel <pamitrovel@gmail.com>
2024-10-27 22:43:15 +0300

    refactor ERROR_NO_CREDENTIALS

--- src/test/java/by/mall/LoginMessage.java
+++ src/test/java/by/mall/LoginMessage.java
@@ -1,8 +1,7 @@
 package by.mall;
 
 public class LoginMessage {
-    public static final String ERROR_NO_CREDENTIALS = "Значение поля \"Номер телефона\" " +
-            "должно начинаться с +375 затем код (25|29|33|44) и далее 7 цифр (первая из которых не 0)\n" +
+    public static final String ERROR_NO_CREDENTIALS = "Поле \"Номер телефона\" обязательно для заполнения.\n" +
             "Поле \"Пароль\" обязательно для заполнения.";
     public static final String ERROR_NO_PASSWORD = "Поле \"Пароль\" обязательно для заполнения.";
     public static final String ERROR_NO_PHONE_NUMBER = "Значение поля \"Номер телефона\" должно начинаться" +
