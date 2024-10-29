202cf11d94b533908fcb510dfb06813e71e6e132
Pavel <pamitrovel@gmail.com>
2024-10-29 21:52:23 +0300

    refactor beforeEach, delete before all

--- src/test/java/by/mall/LoginTest.java
+++ src/test/java/by/mall/LoginTest.java
@@ -8,18 +8,17 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.PageFactory;
 
+import java.time.Duration;
+
 public class LoginTest {
     private static WebDriver driver;
     private static LoginPage loginPage;
 
-    @BeforeAll
-    public static void beforeAll() {
-        driver = new ChromeDriver();
-        loginPage = PageFactory.initElements(driver, LoginPage.class);
-    }
-
     @BeforeEach
     public void beforeEach() {
+        driver = new ChromeDriver();
+        loginPage = PageFactory.initElements(driver, LoginPage.class);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.get("https://emall.by/login/password");
     }
 
